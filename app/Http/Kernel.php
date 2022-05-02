aa10605e948a4d655c829498ac4b0f07bd9b88c5
Sergio Brighenti <sergio.brighenti@everli.com>
2022-05-02 11:26:05 +0200

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,7 +41,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             EnsureFrontendRequestsAreStateful::class,
-            'throttle:120,1',
+            // 'throttle:120,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
