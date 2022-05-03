69b91d3e41985cc220813b4e869a667d6e425f20
SergiX44 <sergio@brighenti.me>
2022-05-03 12:19:24 +0200

    hotfix

--- app/Http/Resources/V2/ApplicationUpdateResource.php
+++ app/Http/Resources/V2/ApplicationUpdateResource.php
@@ -1,5 +1,6 @@
 <?php
 declare(strict_types=1);
+
 namespace App\Http\Resources\V2;
 
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -7,16 +8,32 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class ApplicationUpdateResource extends JsonResource
 {
     /**
+     * @var bool
+     */
+    private $forced = false;
+
+    /**
      * Transform the resource into an array.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
      * @return array
      */
     public function toArray($request)
     {
         return [
+            'forced' => $this->forced,
             'version' => $this->version,
-            'download_url' => url('/applications/' . $this->application->slug . '/' . $this->platform),
+            'download_url' => url(sprintf("/applications/%s/%s", $this->application->slug, $this->platform)),
         ];
+    }
+
+    /**
+     * @param  bool  $flag
+     * @return $this
+     */
+    public function withForcedFlag(bool $flag): ApplicationUpdateResource
+    {
+        $this->forced = $flag;
+        return $this;
     }
 }
